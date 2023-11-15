a2f3a9b40529157f32e8d48f2bf6a621b5ff6e75
Reo-Satooooo <cist.b219.r.satou@gmail.com>
2023-11-16 00:47:26 +0900

    リクエストURLを変更

--- js/main.js
+++ js/main.js
@@ -203,7 +203,7 @@ function fadeOutLoadingScreen() {
 
 async function loadCistBusJson() {
   try {
-    const response = await fetch("http://127.0.0.1:8000/json");
+    const response = await fetch("http://ec2-13-230-179-43.ap-northeast-1.compute.amazonaws.com/json");
     if (!response.ok) {
       throw new Error("Failed to load JSON");
     }
